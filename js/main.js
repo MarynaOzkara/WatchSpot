fe131c0407ae5621e32e6d647f2f170bd91fda7b
MarynaOzkara <ozkaramaryna@gmail.com>
2023-06-26 23:03:49 +0300

    slider counter

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const slider = document.querySelector(".slider-list");
 const prev = document.querySelector(".left");
 const next = document.querySelector(".right");
+const counter = document.querySelector('#counter');
+const sliderList = document.querySelector('.slider-list');
+let counterValue = 1;
+const sliderLength = sliderList.children.length;
+
+console.log(sliderList.children.length);
+
+function updateCounterValue() {
+ counter.textContent = counterValue;
+}
+function countNext() {
+    if(counterValue <= sliderLength - 1) {
+        counterValue++;
+        updateCounterValue();
+    }
+   
+}
+function countPrev() { 
+    if(counterValue <= 1) {
+            counterValue = 1;
+            updateCounterValue();
+        } else {
+            --counterValue;
+    updateCounterValue();
+        }
+    
+}
 
 const mySiema = new Siema ({
     selector: slider,
@@ -17,3 +44,5 @@ function onPrevClick() {
 
 next.addEventListener('click', onNextClick);
 prev.addEventListener('click', onPrevClick);
+next.addEventListener('click', countNext);
+prev.addEventListener('click', countPrev);
